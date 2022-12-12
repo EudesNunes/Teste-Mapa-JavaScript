110562d993b0c914bf15223dd0f9d8e937d270d1
EudesNunes <araujonuneseudes@gmail.com>
2022-12-11 21:51:03 -0300

    Expandir

---
+++ js/app.js
@@ -0,0 +1,36 @@
+window.onload = function inicial() {
+    document.getElementById("ajuste").style.backgroundImage = 'url(../img/mapa/expandir.png)';
+    AjusteMapa = "expandir";
+}
+
+function ajustar() {
+    var urlExpandir = 'url(../img/mapa/expandir.png)';
+    var urlDiminuir = 'url(../img/mapa/diminuir.png)';
+    var divMapa = document.getElementById("Mapa");
+
+    if (AjusteMapa == "diminuir") {
+        document.getElementById("ajuste").style.backgroundImage = urlExpandir;
+        AjusteMapa = "expandir";
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    } else {
+        document.getElementById("ajuste").style.backgroundImage = urlDiminuir;
+        AjusteMapa = "diminuir";
+        if (divMapa.requestFullscreen) {
+            divMapa.requestFullscreen();
+        } else if (elem.mozRequestFullScreen) { /* Firefox */
+            divMapa.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+            divMapa.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE/Edge */
+            divMapa.msRequestFullscreen();
+        }
+    }
+}
